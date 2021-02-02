421e7b5904a167c93b878573dbb2cbd1ff81be8c
Danil <danilugrik19@gmail.com>
2021-02-02 15:56:55 +0700

    complete 2ed task

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let i = 0;
 const personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-let lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
-    raiting = prompt('На сколько оцените его?',''),
-    lastFilmRepeat = prompt('Один из последних просмотренных фильмов?', ''),
-    raitingRepeat = prompt('На сколько оцените его?','');
+if (personalMovieDB.count < 10) {
+    alert('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 & personalMovieDB.count <=30 ) {
+    alert('Вы классический зритель');
+} else if (personalMovieDB.count >=31) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[lastFilm] = raiting;
-personalMovieDB.movies[lastFilmRepeat] = raitingRepeat;
+for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+          raiting = prompt('На сколько оцените его?','');
+    if (lastFilm != null && raiting != null && lastFilm != '' && raiting != '' && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = raiting;
+    } else {
+        i--;
+    }
+}
 
 console.log(personalMovieDB);
